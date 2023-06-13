50fd22eba6f965451af9359aba72b5613ef86f32
felipee lima silva <felipe_lipenew@hotmail.com>
2023-06-13 20:24:14 -0300

    Adicionar arquivos de projeto.

---
+++ Course/Produto.cs
@@ -0,0 +1,66 @@
+﻿using System.Globalization;
+namespace Course
+{
+    internal class Produto
+    {
+        /*ATRIBUTOS*/
+        /*Preco e Quantidade são propriedades automaticas.*/
+
+        private string _nome;
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+
+        /*CONSTRUTORES*/
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        /*PROPRIEDADES*/
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+
+
+
+
+        /*METODOS*/
+
+        public double ValorTotalEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return _nome + ", $" + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", " + Quantidade
+                + " unidades, Toatal: $"
+                + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre com os dados do produto.");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do protudo: " + p);
+
+            Console.WriteLine();
+            Console.Write("Alterar Quantidade? (s/n) :");
+            string respU = Console.ReadLine();
+            if (respU == "s")
+            {
+                Console.Write("Deseja adicionar ou remover produtos?(+/-)");
+                string respD = Console.ReadLine();
+
+                if (respD == "+")
+                {
+                    Console.WriteLine();
+                    Console.Write("Digite a quantidade de produtos a serem adicionados: ");
+                    int qte = int.Parse(Console.ReadLine());
+                    p.AdicionarProdutos(qte);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Dados atualizados: " + p);
+                }
+                else if (respD == "-")
+                {
+                    Console.WriteLine();
+                    Console.Write("Digite a quantidade de produtos a serem removidos: ");
+                    int qte = int.Parse(Console.ReadLine());
+                    p.RemoverProdutos(qte);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Dados atualizados: " + p);
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Dados atualizados: " + p);
+                }
+            }
+            else if (respU == "S")
+            {
+                Console.Write("Deseja adicionar ou remover produtos?(+/-)");
+                string respD = Console.ReadLine();
+
+                if (respD == "+")
+                {
+                    Console.WriteLine();
+                    Console.Write("Digite a quantidade de produtos a serem adicionados: ");
+                    int qte = int.Parse(Console.ReadLine());
+                    p.AdicionarProdutos(qte);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Dados atualizados: " + p);
+                }
+                else if (respD == "-")
+                {
+                    Console.WriteLine();
+                    Console.Write("Digite a quantidade de produtos a serem removidos: ");
+                    int qte = int.Parse(Console.ReadLine());
+                    p.RemoverProdutos(qte);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Dados atualizados: " + p);
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Dados atualizados: " + p);
+                }
+
+            }
+            else if (respU == "n")
+            {
+
+
+
+                Console.WriteLine();
+                Console.WriteLine("Dados atualizados: " + p);
+
+
+            }
+            else if (respU == "N")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Dados atualizados: " + p);
+
+            }
+            else
+            {
+                Console.WriteLine("Comando inexistente!");
+            }
+
+        }
+    }
+
+}
